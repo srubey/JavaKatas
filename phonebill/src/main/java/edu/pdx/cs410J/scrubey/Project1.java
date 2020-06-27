2e43e06a43c2880df20fbf8a97723481e99a2523
srubey <scrubey@pdx.edu>
2020-06-27 16:54:52 -0700

    Created lists for args and options; working on error handling ph number

--- phonebill/src/main/java/edu/pdx/cs410J/scrubey/Project1.java
+++ phonebill/src/main/java/edu/pdx/cs410J/scrubey/Project1.java
@@ -1,17 +1,51 @@
 package edu.pdx.cs410J.scrubey;
 
+import java.util.ArrayList;
+
 /**
  * The main class for the CS410J Phone Bill Project
  */
 public class Project1 {
 
   public static void main(String[] args) {
+    ArrayList<String> CLargs = new ArrayList<>();  //command line arguments list
+    ArrayList<String> CLopts = new ArrayList<>();  //command line options list
     PhoneCall call = new PhoneCall();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
+
+    //add CL args and options to appropriate list
     for (String arg : args) {
-      System.out.println(arg);
+      if(arg.startsWith("-"))
+        CLopts.add(arg);
+      else
+        CLargs.add(arg);
     }
-    System.exit(1);
+
+    //sanity check
+    System.out.print(CLargs);
+    System.out.print(CLopts);
+
+    //print error message if incorrect number of CL arguments
+    if(CLargs.size() < 5)
+      System.err.println("\nMissing command line arguments");
+    if(CLargs.size() > 5)
+      System.err.println("\nToo many command line arguments");
+
+    //check for proper phone number format in 2nd and 3rd arguments
+    //if format incorrect, print error and exit program
+    for(int i = 1; i < 3; ++i) {
+      if (!checkPhNumFormat(CLargs.get(i))){
+        System.err.print("Phone number entered improperly");
+        System.exit(1);
+      }
+    }
   }
 
+  public static boolean checkPhNumFormat(String ph){
+    boolean pass = false;
+    for(int i = 0; i < 12; ++i){
+      
+    }
+
+    return pass;
+  }
 }
--- phonebill/src/test/java/edu/pdx/cs410J/scrubey/Project1Test.java
+++ phonebill/src/test/java/edu/pdx/cs410J/scrubey/Project1Test.java
@@ -28,4 +28,9 @@ public class Project1Test {
       assertThat(line, containsString("This is a README file!"));
     }
   }
+
+  public void lessThan5CLargsFails(){
+    String[] args = new String[]{"one", "two", "three", "four", "five"};
+
+  }
 }
