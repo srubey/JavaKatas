021f2cb8e86c9173c6bf4893f29f09cd43a7d687
srubey <scrubey@pdx.edu>
2020-07-02 12:33:17 -0700

    Completed javadoc comments for PhoneCall.java, as well as all unit test classes.

---
+++ quiz1/src/Functions.java
@@ -0,0 +1,9 @@
+public class Functions {
+    public int sum(int n){
+        int sum = 0;
+        for(int i = 1; i <= n; ++i)
+            sum += i;
+
+        return sum;
+    }
+}
---
+++ quiz1/src/Main.java
@@ -0,0 +1,15 @@
+import java.lang.Character.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*        for(int i = 0; i < 11; i += 2){
+            System.out.print(i + "\n");
+        }  */
+        Functions f = new Functions();
+        //System.out.print(f.sum(5));
+
+        System.out.print(Float.MIN_VALUE + "\n");
+        System.out.print(Float.MAX_VALUE);
+    }
+}
