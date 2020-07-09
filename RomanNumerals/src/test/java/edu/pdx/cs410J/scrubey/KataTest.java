a882ebefb976a904241fcdaa1e73d71369b284ec
srubey <scrubey@pdx.edu>
2020-07-09 10:32:32 -0700

    Created new unit tests.

--- RomanNumerals/src/test/java/edu/pdx/cs410J/scrubey/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/scrubey/KataTest.java
@@ -15,14 +15,63 @@ public class KataTest
   @Test
   public void checkForThousands(){
     Kata kata = new Kata();
-    int input = 2668;
+    int input = 3001;
 
-    assertThat(kata.intToRomanNum(input), equalTo("MMDCLXVIII"));
+    assertThat(kata.intToRomanNum(input), equalTo("MMMI"));
   }
   @Test
   public void checkForTwoThousands(){
     Kata kata = new Kata();
     int input = 2001;
     assertThat(kata.intToRomanNum(input), equalTo("MMI"));
+  }
+
+  @Test
+  public void checkFor1(){
+    Kata kata = new Kata();
+    int input = 1;
+    assertThat(kata.intToRomanNum(input), equalTo("I"));
+  }
+
+  @Test
+  public void checkFor5(){
+    Kata kata = new Kata();
+    int input = 5;
+    assertThat(kata.intToRomanNum(input), equalTo("V"));
+  }
+
+  @Test
+  public void checkFor10(){
+    Kata kata = new Kata();
+    int input = 10;
+    assertThat(kata.intToRomanNum(input), equalTo("X"));
+  }
+
+  @Test
+  public void checkFor50(){
+    Kata kata = new Kata();
+    int input = 50;
+    assertThat(kata.intToRomanNum(input), equalTo("L"));
+  }
+
+  @Test
+  public void checkFor100(){
+    Kata kata = new Kata();
+    int input = 100;
+    assertThat(kata.intToRomanNum(input), equalTo("C"));
+  }
+
+  @Test
+  public void checkFor500(){
+    Kata kata = new Kata();
+    int input = 500;
+    assertThat(kata.intToRomanNum(input), equalTo("D"));
+  }
+
+  @Test
+  public void checkFor1000(){
+    Kata kata = new Kata();
+    int input = 1000;
+    assertThat(kata.intToRomanNum(input), equalTo("M"));
   }
 }
